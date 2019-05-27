cfa4cf7f89f7368802d166515503a5606a77987d
jayapriyak <47291244+jayapriyak@users.noreply.github.com>
2019-05-27 16:53:52 +0530

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,11 @@
+marks=79
+if marks>=80 and marks<=100:
+    print("A")
+elif marks>=73 and marks<=79:
+    print("B")
+elif marks>=65 and marks<=72:
+    print("C")
+elif marks>=0 and marks<=64:
+    print("D")
+else:
+    print("Z")
